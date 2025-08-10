757d8a29cb759d0f9bac005ffb1de666c2374a75
daeun <jaeunda@gmail.com>
2025-08-10 23:38:22 +0900

    refactor(log): simplify logging utility functions (#3)

--- src/include/util/log.h
+++ src/include/util/log.h
@@ -3,19 +3,24 @@
 
 #include <stdio.h>
 
-typedef enum {
-    LOG_LEVEL_DEBUG = 0,
-    LOG_LEVEL_INFO,
-    LOG_LEVEL_WARN,
-    LOG_LEVEL_ERROR,
-    LOG_LEVEL_FATAL,
-    LOG_LEVEL_NONE  // disable all logging
-} LogLevel;
+// typedef enum {
+//     LOG_LEVEL_DEBUG = 0,
+//     LOG_LEVEL_INFO,
+//     LOG_LEVEL_WARN,
+//     LOG_LEVEL_ERROR,
+//     LOG_LEVEL_FATAL,
+//     LOG_LEVEL_NONE  // disable all logging
+// } LogLevel;
 
-void log_init(const char *file_path, LogLevel level);
-void log_set_level(LogLevel level);
-LogLevel log_get_level(void);
-void log_msg(LogLevel level, const char *fmt, ...);
-void log_close(void);
+// void log_init(const char *file_path, LogLevel level);
+// void log_set_level(LogLevel level);
+// LogLevel log_get_level(void);
+// void log_msg(LogLevel level, const char *fmt, ...);
+// void log_close(void);
+
+// stdout
+void log_msg(const char *format, ...); 
+// stderr and exit
+void log_error(const char *format, ...);
 
 #endif
