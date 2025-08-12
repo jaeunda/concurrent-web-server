6d69e39b1f9ea3c68cc05c2812c2d843c676400d
daeun <jaeunda@gmail.com>
2025-08-12 22:21:59 +0900

    refactor(log): update log.h to follow header-guidelines #3 #6

--- src/include/util/log.h
+++ src/include/util/log.h
@@ -6,25 +6,25 @@
 #include <stdarg.h>
 #include <time.h>
 
-// typedef enum {
-//     LOG_LEVEL_DEBUG = 0,
-//     LOG_LEVEL_INFO,
-//     LOG_LEVEL_WARN,
-//     LOG_LEVEL_ERROR,
-//     LOG_LEVEL_FATAL,
-//     LOG_LEVEL_NONE  // disable all logging
-// } LogLevel;
+typedef enum {
+    LOG_LEVEL_DEBUG = 0,
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_WARN,
+    LOG_LEVEL_ERROR,
+    LOG_LEVEL_FATAL,
+    LOG_LEVEL_NONE  // disable all logging
+} LogLevel;
 
-// void log_init(const char *file_path, LogLevel level);
+void log_init(const char *file_path); // if file_path == NULL then console
+void log_set_console(int enabled);
+
 // void log_set_level(LogLevel level);
-// LogLevel log_get_level(void);
-// void log_msg(LogLevel level, const char *fmt, ...);
-// void log_close(void);
 
-static void get_timestamp(char *buffer, size_t size);
-// stdout
-void log_msg(const char *format, ...); 
-// stderr and exit
-void log_error(const char *format, ...);
+void log_msg(const char *format, ...); // stdout
+void log_error(const char *format, ...); // stderr
+void log_fatal(const char *format, ...); // stderr -> exit
+
+void log_close(void);
+
 
 #endif
