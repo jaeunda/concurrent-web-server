8374903beae247773ae392f337a68a51557d2514
daeun <jaeunda@gmail.com>
2025-08-16 02:26:47 +0900

    refactor(server): change main function signature #10

--- src/servers/server-thread-pool.c
+++ src/servers/server-thread-pool.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-int main(void){
+int main(int argc, char **argv){
     printf("[%d] ThreadPool Server Running ...\n", getpid());
     for (int i=3; i>0; i--){
         printf("[%d] Close in %d seconds ...\n", getpid(), i);
