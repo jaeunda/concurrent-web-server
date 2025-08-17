f16aa6a240bd4979296b25bbfad03542d6ee0053
daeun <jaeunda@gmail.com>
2025-08-18 00:10:15 +0900

    chore: restore previous work #3

--- src/util/log.c
+++ src/util/log.c
@@ -1,9 +1,23 @@
-#include "../include/util/log.h"
+#include "log.h"
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #include <stdarg.h>
+
 #include <time.h>
+#include <sys/syscall.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#if defined(_MODE_THREAD) || defined(_MODE_THREADPOOL)
+#include <pthread.h> // mutex provides exclusive access to the file, preventing race conditions
+#endif
+
+#ifdef __linux__ // pre-defined by the compiler on Linux
+#include <sys/file.h> // file locking
+#endif
 
 static void get_timestamp(char *, size_t);
 
