3f8efa900ce2b7fbb766579a9b88fd0887c5446a
daeun <jaeunda@gmail.com>
2025-08-17 15:38:24 +0900

    feat: include ip and port in server start message #10

--- src/servers/server-multi-thread.c
+++ src/servers/server-multi-thread.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 int main(int argc, char **argv){
-    printf("[%d] Multi-Threaded Server Running ...\n", getpid());
+    printf("[%d] Multi-Threaded Server Running on %s:%s...\n", getpid(), argv[2], argv[1]);
     for (int i=3; i>0; i--){
         printf("[%d] Close in %d seconds ...\n", getpid(), i);
         sleep(1);
