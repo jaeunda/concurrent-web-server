4b690cd8ca6a8c8dff05b296a5caaa88745df080
daeun <jaeunda@gmail.com>
2025-08-17 16:10:45 +0900

    feat: add error handling for invalid number of argument #10

--- src/servers/server-multi-thread.c
+++ src/servers/server-multi-thread.c
@@ -3,6 +3,10 @@
 #include <unistd.h>
 #include <string.h>
 int main(int argc, char **argv){
+    if (argc != 5){
+        fprintf(stderr, "Error: argument error\n");
+        exit(EXIT_FAILURE);
+    }
     printf("[%d] Multi-Threaded Server Running on %s:%s...\n", getpid(), argv[2], argv[1]);
     for (int i=3; i>0; i--){
         printf("[%d] Close in %d seconds ...\n", getpid(), i);
